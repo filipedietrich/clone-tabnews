f2366b90cd14ccd70f1eac4861b0799ffb8ce190
filipedietrich <mattos.filipe@hotmail.com>
2023-07-21 12:26:45 -0300

    adiciona scripts ` lint:check`, `lint:fix` e `.lintignore`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Fiz essa página com os conhecimentos adquiridos no curso.dev</h1>
+  return <h1>Fiz essa página com os conhecimentos adquiridos no curso.dev</h1>;
 }
 
 export default Home;
